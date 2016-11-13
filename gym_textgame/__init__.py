235b3183a3b14e314698a58ad3f78e45d5535cc7
Rene Knaebel <reneknaebel90@gmail.com>
2016-11-13 11:43:45 +0100

    added new env to registration

--- gym_textgame/__init__.py
+++ gym_textgame/__init__.py
@@ -10,3 +10,11 @@ register(
     reward_threshold=1.0,
     nondeterministic = False,
 )
+
+register(
+    id='HomeWorldHard-v0',
+    entry_point='gym_textgame.envs:HomeWorldEnv2',
+    timestep_limit=100,
+    reward_threshold=1.0,
+    nondeterministic = False,
+)
--- gym_textgame/envs/__init__.py
+++ gym_textgame/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_textgame.envs.textgame_env import HomeWorldEnv
+from gym_textgame.envs.textgame_env2 import HomeWorldEnv2
